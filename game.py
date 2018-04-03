33963ad7574f832c18807a5d0f66d3aa363d0251
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 16:18:04 -0700

    trouble shooting player inp

--- game.py
+++ game.py
@@ -7,23 +7,28 @@ rand_num = random.randint(1,100)
 print '%s, I\'m thinking of a number between 1 and 100. \
 \nTry to guess my number' % player_name
 
+# player guess as string
+
 
 counter = 0
 guess = True
 
 while guess:
-    player_guess = int(raw_input('Your guess?'))
-    if isinstance(player_guess, int):
-        if player_guess == rand_num:
-            print 'Well done, %s! You found my number in %s tries!' % (player_name, counter)
-            guess = False
-        elif player_guess > rand_num:
-            print 'Your guess is too high, try again.'
-            counter += 1    
-        elif player_guess < rand_num:
-            print 'Your guess is too low, try again.'
-            counter += 1
-        else:
-            print '%s! Check the range please!' % player_name
+    player_guess = raw_input('Your guess?')
+
+    if int(player_guess):
+        player_guess = int(player_guess)
+        if player_guess :
+            if player_guess == rand_num:
+                print 'Well done, %s! You found my number in %s tries!' % (player_name, counter)
+                guess = False
+            elif player_guess > rand_num:
+                print 'Your guess is too high, try again.'
+                counter += 1    
+            elif player_guess < rand_num:
+                print 'Your guess is too low, try again.'
+                counter += 1
+            else:
+                print '%s! Check the range please!' % player_name
     else:
         print 'error'
